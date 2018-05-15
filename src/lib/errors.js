e6192cb3e2d8edeea0d1adfa893abf526d6533bc
emi <emiliorizzo@gmail.com>
2018-05-15 13:24:42 -0300

    Change: errors -> types.errors

--- src/lib/errors.js
+++
@@ -1,3 +0,0 @@
-export const INVALID_REQUEST = 'Invalid Request'
-export const INVALID_TYPE = 'Invalid Type'
-export const EMPTY_RESULT = 'Not Found'
---
+++ src/lib/types.js
@@ -0,0 +1,15 @@
+
+export const txTypes = {
+  default: 'normal',
+  remasc: 'remasc',
+  bridge: 'bridge',
+  contract: 'contract deploy'
+}
+
+export const errors = {
+  INVALID_REQUEST: 'Invalid Request',
+  INVALID_TYPE: 'Invalid Type',
+  EMPTY_RESULT: 'Not Found'
+}
+
+export default { txTypes, errors }
