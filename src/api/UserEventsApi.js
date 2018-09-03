07045b7e606d43af467ec58954afcbbb73c372dc
emi <emiliorizzo@gmail.com>
2018-09-03 03:29:15 -0300

    Refactor UserEventsApi

--- src/api/UserEventsApi.js
+++ src/api/UserEventsApi.js
@@ -15,25 +15,37 @@ export const UserEventsApi = (io, blocks, log) => {
   userEvents.on('message', msg => {
     const socket = io.sockets.connected[msg.socketId]
     if (socket) {
-      let params, payload, error
-      ({ params, payload, error } = msg)
+      const payload = msg.payload
       const action = payload.action
-      if (!error) {
-        if (msg.data) {
-          socket.emit('data', formatRes(action, msg, payload))
-        } else {
-          blocks.run(action, params).then(result => {
-            socket.emit('data', formatRes(action, result, payload))
-          })
-        }
-      } else {
-        socket.emit('data', formatRes(action, null, payload, errors.INVALID_REQUEST))
-      }
+      processMsg(action, msg, blocks)
+        .then(res => {
+          console.log(res)
+          socket.emit('data', formatRes(action, res.data, payload, res.error))
+        }).catch(err => {
+          log.error(err)
+        })
     } else {
       log.error(`Socket id: ${msg.socketId} not found`)
     }
   })
   return userEvents
+}
+
+async function processMsg (action, msg, blocks) {
+  let data, error
+  if (!msg.error) {
+    if (msg.data) {
+      data = msg
+    } else {
+      data = await blocks.run(action, msg.params).then(result => {
+        return result
+      })
+    }
+  } else {
+    error = errors[msg.error.code] || errors.INVALID_REQUEST
+    data = msg.result
+  }
+  return { data, error }
 }
 
 export default UserEventsApi
