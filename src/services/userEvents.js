fc838698c1d86ace2b9f8361dcf51e3be35ae817
emi <emiliorizzo@gmail.com>
2018-09-04 20:04:43 -0300

    Fix userEvents, serialize msg before send
    Fix cached BigNumber fields

--- src/services/userEvents.js
+++ src/services/userEvents.js
@@ -4,6 +4,7 @@ import web3Connect from '../lib/web3Connect'
 import Address from './classes/Address'
 import Logger from '../lib/Logger'
 import { errors } from '../lib/types'
+import { serialize } from '../lib/utils'
 
 const config = Object.assign({}, conf.blocks)
 const log = Logger('UserRequests', config.log)
@@ -23,35 +24,35 @@ dataSource.then(db => {
             const cached = cache.isRequested(action, address, block)
             if (cached) {
               msg.data = cached
-              process.send(msg)
+              sendMessage(msg)
             } else {
               const Addr = new Address(address, web3, addressCollection)
               Addr.fetch().then(result => {
                 msg.result = result
-                cache.set(action, address, result)
-                let balance = (result.balance) ? result.balance.toString() : 0
+                cache.set(action, address, result, block)
+                const balance = (result.balance) ? result.balance.toString() : 0
                 if (balance > 0) {
                   Addr.save()
                     .catch(err => {
                       log.error(`Error saving address ${address}, ${err}`)
                     })
-                    .finally(
-                      process.send(msg)
-                    )
+                    .finally(() => {
+                      sendMessage(msg)
+                    })
                 } else {
                   msg.data = result
-                  process.send(msg)
+                  sendMessage(msg)
                 }
               }).catch(err => {
                 log.error(err)
                 msg.error = errors.TEMPORARILY_UNAVAILABLE
-                process.send(msg)
+                sendMessage(msg)
               })
             }
           } catch (err) {
             log.debug(err)
             msg.error = err
-            process.send(msg)
+            sendMessage(msg)
           }
           break
       }
@@ -59,12 +60,16 @@ dataSource.then(db => {
   })
 })
 
+const sendMessage = (msg) => {
+  process.send(serialize(msg))
+}
 class RequestCache {
   constructor () {
     this.requested = {}
     this.block = null
   }
-  set (action, key, value) {
+  set (action, key, value, block) {
+    this.setBlock(block)
     let actions = this.getAction(action)
     actions[key] = value
     this.requested[action] = actions
