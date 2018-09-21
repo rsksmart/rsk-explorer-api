275047f8f4e0800bef33ab5833be19ffee569ad3
emi <emiliorizzo@gmail.com>
2018-09-21 12:57:40 +0000

    Add tools/checkBlocksCongruence

---
+++ src/lib/cli.js
@@ -0,0 +1,10 @@
+export const reset = '\x1b[0m'
+export const red = '\x1b[31m'
+export const blue = '\x1b[36m'
+export const green = '\x1b[32m'
+export const orange = '\x1b[33m'
+
+export const error = l => console.log(red, l, reset)
+export const warn = l => console.log(orange, l, reset)
+export const info = l => console.log(blue, l, reset)
+export const ok = l => console.log(green, l, reset)
---
+++ src/tools/checkBlocksCoungruence.js
@@ -0,0 +1,26 @@
+import dataSource from '../lib/dataSource.js'
+import conf from '../lib/config'
+import fs from 'fs'
+import util from 'util'
+import { checkBlocksCongruence } from '../lib/utils'
+const config = Object.assign({}, conf.blocks)
+const writeFile = util.promisify(fs.writeFile)
+const outFile = '/tmp/blocksLog.json'
+dataSource.then(async db => {
+  try {
+    const Blocks = db.collection(config.collections.Blocks)
+    console.log('Getting blocks....')
+    let res = await checkBlocksCongruence(Blocks)
+    res.missingTotal = res.missing.length
+    res.invalidTotal = res.invalid.length
+    console.log(`Missing Blocks:  ${res.missingTotal}`)
+    console.log(`Invalid Blocks: ${res.invalidTotal}`)
+    await writeFile(outFile, JSON.stringify(res))
+
+    console.log(`Log saved on: ${outFile}`)
+    process.exit(0)
+  } catch (err) {
+    console.log(err)
+    process.exit(9)
+  }
+})
