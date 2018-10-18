b12178288da4e779ccdca2ae82c6ea74d9eef0fd
emi <emiliorizzo@gmail.com>
2018-10-18 02:25:11 +0000

    Update tools/statusClient

--- src/tools/statusClient.js
+++ src/tools/statusClient.js
@@ -2,9 +2,10 @@ import io from 'socket.io-client'
 import config from '../lib/config'
 import { info, ok, warn, red, green, orange, reset, error } from '../lib/cli'
 const url = process.env.URL || `ws://localhost:${config.server.port}`
-
-
 const socket = io.connect(url, { reconnect: true })
+let blocksPerSecond
+let stats = { time: 0, blocks: 0 }
+
 info(`Waiting for: ${url}`)
 
 socket.on('connect', socket => {
@@ -19,7 +20,14 @@ socket.on('data', data => {
   let action = data.action
   if (action === 'dbStatus' && data.data) {
     const status = data.data
-    delete (status.missingSegments)
+    const prevState = status.prevState
+    if (prevState && prevState.dbTime) {
+      stats.time += parseInt((status.dbTime - prevState.dbTime) / 1000)
+      stats.blocks += status.dbBlocks - prevState.dbBlocks
+      blocksPerSecond = (stats.blocks / stats.time).toFixed(1)
+    }
+    delete status.missingSegments
+    delete status.prevState
     console.clear()
     console.log()
     info(url)
@@ -29,6 +37,12 @@ socket.on('data', data => {
     console.log(`   Db   ${(status.dbMissingBlocks > 0) ? red : (status.requestingBlocks > 5) ? orange : green} ● ${reset}`)
     console.log()
     console.dir(status, { colors: true })
+    if (blocksPerSecond) {
+      let color = (blocksPerSecond < 10) ? red : (blocksPerSecond < 20) ? orange : green
+      console.log()
+      console.log(`${color} ${blocksPerSecond}${reset} B/s`)
+      console.log(`${color} ${parseInt(blocksPerSecond * 3600)}${reset} B/h`)
+    }
     if (status.nodeDown) error('The node is down... ☹ ')
   }
 })
