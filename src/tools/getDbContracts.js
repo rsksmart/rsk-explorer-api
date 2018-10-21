39271d09323c05b7d3cb93b74d91dec3e297ce06
emi <emiliorizzo@gmail.com>
2018-10-21 17:55:36 +0000

    update tools

---
+++ src/tools/getDbContracts.js
@@ -0,0 +1,14 @@
+import dataSource from '../lib/dataSource.js'
+
+
+dataSource.then(db => {
+  let addresses = db.collection('addresses')
+  addresses.find({ type: 'contract' })
+    .project({ _id: 0, address: 1 })
+    .toArray()
+    .then(res => {
+      console.log(JSON.stringify(res))
+      process.exit(0)
+    }
+    )
+})
