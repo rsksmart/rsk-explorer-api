2b229ddb4912f6c709a1dc73ecbf4aaa097db303
emi <emiliorizzo@gmail.com>
2018-11-08 11:50:14 -0300

    Update tools, add getFailedDeployments

---
+++ src/tools/getFailedDeployments.js
@@ -0,0 +1,21 @@
+import dataSource from '../lib/dataSource.js'
+import conf from '../lib/config'
+const config = Object.assign({}, conf.blocks)
+dataSource.then(async db => {
+  try {
+    const Addrs = db.collection(config.collections.Addrs)
+    let result = await Addrs.find({ createdByTx: { $exists: true }, type: 'account' })
+      .project({ address: 1, type: 1, _id: 0 })
+      .toArray()
+    if (result) {
+      result = result.map(r => r.address)
+      console.log(JSON.stringify(result))
+    } else {
+      console.log('No results')
+    }
+    process.exit(0)
+  } catch (err) {
+    console.error(err)
+    process.exit(9)
+  }
+})
