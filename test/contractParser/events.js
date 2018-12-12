e8eb78b036a5e945dd5d9d9f4de3d0734fe826f7
emi <emiliorizzo@gmail.com>
2018-12-12 00:34:13 -0300

    Update tests

--- test/contractParser/events.js
+++ test/contractParser/events.js
@@ -1,6 +1,8 @@
 import { assert } from 'chai'
 import { ContractParser } from '../../src/lib/ContractParser/ContractParser'
+import { serialize } from '../../src/lib/utils'
 import tx1 from './txs/01.json'
+import tx2 from './txs/02.json'
 
 const txs = [
   {
@@ -16,6 +18,53 @@ const txs = [
         }
       ]
     }
+  },
+  {
+    tx: tx2.result,
+    expect: {
+      events: [
+        {
+          event: 'Transfer',
+          args: {
+            from: '0xda1683aa3a4d16f76bcd3ef2c209ba332e533f4d',
+            to: '0xb92675ccf00728fee1390a5d0d4ca594ecfb5c1f',
+            value: '0xfd5b62ad0505ecaed7000'
+          }
+        },
+        {
+          event: 'Transfer',
+          args: {
+            from: '0xda1683aa3a4d16f76bcd3ef2c209ba332e533f4d',
+            to: '0xd5ae806315937698db2b9c13bbfc149e59dcda80',
+            value: '0x21c7eb0600ab814f06c00'
+          }
+        },
+        {
+          event: 'Transfer',
+          args: {
+            from: '0xda1683aa3a4d16f76bcd3ef2c209ba332e533f4d',
+            to: '0xe4e962e97998a976a337752c978eff857379850c',
+            value: '0x10e3f5830055d347e2800'
+          }
+        },
+        {
+          event: 'Transfer',
+          args: {
+            from: '0xda1683aa3a4d16f76bcd3ef2c209ba332e533f4d',
+            to: '0x2d9bea5f54e315dc40a14d4a2196687a15c6f9b2',
+            value: '0x19d51d22b537ece26dc00'
+          }
+        },
+        {
+          event: 'Transfer',
+          args: {
+            from: '0xda1683aa3a4d16f76bcd3ef2c209ba332e533f4d',
+            to: '0x517cd1c1ff08ab6a1c7ec742a8c51cc0f09954e5',
+            value: '0x511300db34ceedbe4200'
+          }
+        }
+      ]
+    }
   }
 ]
 
@@ -27,30 +76,32 @@ describe('# decode events', function () {
     let e = t.expect
     describe(`TX: ${tx.transactionHash}`, function () {
       let decodedLogs = parser.parseTxLogs(tx.logs)
+      decodedLogs = decodedLogs.map(log => serialize(log))
 
       it(`should return ${e.events.length} events`, function () {
         assert.equal(tx.logs.length, e.events.length)
       })
 
       for (let i = 0; i < e.events.length; i++) {
-        let event = e.events[i]
-        let decoded = decodedLogs[i]
-        it(`should be ${event.event} event`, function () {
-          assert.equal(event.event, decoded.event)
-        })
-
-        let argsLength = Object.keys(event.args).length
-        it(`should have ${argsLength} arguments`, function () {
-          assert.equal(argsLength, Object.keys(decoded.args).length)
-        })
-
-        for (let a in event.args) {
-          let arg = event.args[a]
-          it(`should have: ${a} arg with value: ${arg}`, function () {
-            assert.propertyVal(decoded.args, a, arg)
+        describe(`Event ${i}`, function () {
+          let event = e.events[i]
+          let decoded = decodedLogs[i]
+          it(`should be ${event.event} event`, function () {
+            assert.equal(event.event, decoded.event)
           })
-        }
 
+          let argsLength = Object.keys(event.args).length
+          it(`should have: ${argsLength} arguments`, function () {
+            assert.equal(argsLength, Object.keys(decoded.args).length)
+          })
+
+          for (let a in event.args) {
+            let arg = event.args[a]
+            it(`should have: ${a} arg with value: ${arg}`, function () {
+              assert.propertyVal(decoded.args, a, arg)
+            })
+          }
+        })
       }
 
     })
---
+++ test/services/getBlock.spec.js
@@ -0,0 +1,49 @@
+
+import { expect, assert } from 'chai'
+import { Block } from '../../src/services/classes/Block'
+import { BlocksBase } from '../../src/lib/BlocksBase'
+import { nod3 } from '../../src/lib/nod3Connect'
+import datasource from '../../src/lib/dataSource'
+
+import blockJson from './block-35162.json'
+const blockSpec = blockJson.block
+
+
+
+describe('Get Block', function () {
+  let blockData
+  let blockNumber = 35162
+  it('should be connected to RSK testnet', async function () {
+    let net = await nod3.net.version()
+    expect(net.id).to.be.equal('31')
+  })
+  it(`should get block ${blockNumber}`, async function () {
+    this.timeout(60000)
+    let db = await datasource
+    let block = new Block(blockNumber, new BlocksBase(db))
+    await block.fetch()
+    blockData = block.getData(true)
+    expect(blockData).to.be.an('object')
+  })
+
+  it('should have block properties', function () {
+    expect(Object.keys(blockData)).to.be.deep.equal(Object.keys(blockSpec))
+  })
+
+  it(`tokenAddresses`, function () {
+    expect(blockData.tokenAddresses.length).to.be.equal(blockSpec.tokenAddresses.length)
+  })
+
+  for (let k = 0; k < blockSpec.tokenAddresses.length; k++) {
+    for (let p of ['address', 'contract', 'balance']) {
+      it(`should have ${p} property`, () => {
+        expect(blockData.tokenAddresses[k]).has.property(p)
+      })
+
+      it('should have expected values', function () {
+        expect(blockData.tokenAddresses[k].address).to.be.equal(blockSpec.tokenAddresses[k].address)
+        expect(blockData.tokenAddresses[k].contract).to.be.equal(blockSpec.tokenAddresses[k].contract)
+      })
+    }
+  }
+})
