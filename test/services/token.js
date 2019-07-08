23ec57befd4f1e8fc2043ae43ded3ef7ae0340ef
emi <emiliorizzo@gmail.com>
2019-07-08 20:37:41 +0000

    Update tests

--- test/services/token.js
+++ test/services/token.js
@@ -5,17 +5,16 @@ import { BlocksBase } from '../../src/lib/BlocksBase'
 import { nod3 } from '../../src/lib/nod3Connect'
 import datasource from '../../src/lib/dataSource'
 
-describe('TST token', function () {
+describe('Test token', function () {
   let blockData, contract
   it('should be connected to RSK testnet', async function () {
     let net = await nod3.net.version()
-    console.log(net)
     expect(net.id).to.be.equal('31')
   })
-  it('should create a contract', async function () {
+  it('should get a contract', async function () {
     this.timeout(60000)
     let db = await datasource
-    let block = new Block(22256, new BlocksBase(db))
+    let block = new Block(8308, new BlocksBase(db))
     await block.fetch()
     blockData = block.getData(true)
     expect(blockData.contracts).to.be.an('array')
@@ -23,13 +22,12 @@ describe('TST token', function () {
   })
 
   it('should be an ERC677 token', function () {
-    expect(contract.contractInterfaces).include.members(['ERC20', 'ERC677'])
+    expect(contract.contractInterfaces).includes.members(['ERC165', 'ERC721'])
   })
 
   it('should be TST token', function () {
-    expect(contract.name).to.be.equal('TST')
-    expect(contract.symbol).to.be.equal('TST')
-    expect(contract.decimals).to.be.equal('0x12')
-    expect(contract.totalSupply).to.be.equal('0x33b2e3c9fd0803ce8000000')
+    expect(contract.name).to.be.equal('Argentinos Juniors Coin')
+    expect(contract.symbol).to.be.equal('ARGJ')
+    // expect(contract.totalSupply).to.be.equal('0x10')
   })
 })
