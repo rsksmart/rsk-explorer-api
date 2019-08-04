87a1cb3c0945167294334721b33bdfe88a7a67af
emi <emiliorizzo@gmail.com>
2019-08-04 07:47:19 +0000

    Update services/blocks to use the setup module

--- src/services/blocks/index.js
+++ src/services/blocks/index.js
@@ -1,17 +1,55 @@
 import path from 'path'
 import { fork } from 'child_process'
-import { dataBase } from '../../lib/dataSource.js'
 import conf from '../../lib/config'
-import blocksCollections from '../../lib/collections'
 import Logger from '../../lib/Logger'
 import { BlocksStatus } from '../classes/BlocksStatus'
 import { BcStats } from '../classes/BcStats'
 import { actions } from '../../lib/types'
+import Setup from '../../lib/Setup'
 
 const config = Object.assign({}, conf.blocks)
 const log = Logger('Blocks', config.log)
 config.Logger = log
-dataBase.setLogger(log)
+
+startBlocks()
+
+async function startBlocks () {
+  const setup = await Setup(log)
+  await setup.createCollections()
+  const { db } = await setup.start()
+  const Status = new BlocksStatus(db, config)
+  const Stats = new BcStats(db, config)
+  const listenToMessage = (msg, service) => {
+    let action, args, event, data
+    ({ action, args, event, data } = msg)
+    if (event) {
+      readEvent(event, data)
+    }
+    if (action) {
+      switch (action) {
+        case actions.STATUS_UPDATE:
+          Status.update(...args)
+          break
+
+        case actions.BLOCK_REQUEST:
+        case actions.BULK_BLOCKS_REQUEST:
+          Requester.send({ action, args })
+          break
+
+        case actions.UPDATE_TIP_BLOCK:
+          Stats.update(...args)
+          Checker.send({ action, args })
+          break
+      }
+    }
+  }
+  /* eslint-disable-next-line no-unused-vars */
+  const Listener = startService('Listener', listenToMessage)
+  const Checker = startService('Checker', listenToMessage)
+  const Requester = startService('Requester', listenToMessage)
+  /* eslint-disable-next-line no-unused-vars */
+  const TxPool = startService('TxPool', listenToMessage, '../txPool.js')
+}
 
 function startService (name, parseMessage, script) {
   script = script || `blocks${name}.js`
@@ -21,59 +59,9 @@ function startService (name, parseMessage, script) {
   return service
 }
 
-dataBase.db().then(db => {
-  createBlocksCollections(config, db).then(() => {
-    const Status = new BlocksStatus(db, config)
-    const Stats = new BcStats(db, config)
-    const listenToMessage = (msg, service) => {
-      let action, args, event, data
-      ({ action, args, event, data } = msg)
-      if (event) {
-        readEvent(event, data)
-      }
-      if (action) {
-        switch (action) {
-          case actions.STATUS_UPDATE:
-            Status.update(...args)
-            break
-
-          case actions.BLOCK_REQUEST:
-          case actions.BULK_BLOCKS_REQUEST:
-            Requester.send({ action, args })
-            break
-
-          case actions.UPDATE_TIP_BLOCK:
-            Stats.update(...args)
-            Checker.send({ action, args })
-            break
-        }
-      }
-    }
-    /* eslint-disable-next-line no-unused-vars */
-    const Listener = startService('Listener', listenToMessage)
-    const Checker = startService('Checker', listenToMessage)
-    const Requester = startService('Requester', listenToMessage)
-    /* eslint-disable-next-line no-unused-vars */
-    const TxPool = startService('TxPool', listenToMessage, '../txPool.js')
-  })
-})
-
 // WIP
 const readEvent = (event, data) => {
   log.info(event, data)
-}
-
-async function createBlocksCollections (config, db) {
-  try {
-    let names = config.collections
-    let validate = config.validateCollections
-    let options = { names, validate }
-    await dataBase.createCollections(blocksCollections, options)
-  } catch (err) {
-    log.error('Error creating blocks')
-    log.error(err)
-    process.exit(9)
-  }
 }
 
 process.on('unhandledRejection', err => {
