689516b6e0463e4bdeda632cbfffc1cdbac3ca89
emi <emiliorizzo@gmail.com>
2019-12-22 17:29:27 -0300

    Change config, export config factory

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,51 +2,59 @@ import path from 'path'
 import fs from 'fs'
 import defaultConf from './defaultConfig'
 
-const keys = Object.keys(defaultConf)
-const config = loadConfig()
-for (let key of keys) {
-  config[key] = config[key] || defaultConf[key]
-  for (let p in defaultConf[key]) {
-    if (undefined === config[key][p]) config[key][p] = defaultConf[key][p]
+export const config = createConfig('../../config.json')
+
+export function createConfig (file) {
+  const defaultLogs = (key) => {
+    const dir = config.log.dir
+    if (!dir) return
+    config[key].log = config[key].log || {}
+    config[key].log.file = config[key].log.file || `${dir}/${key}.json`
+    config[key].log.level = config[key].log.level || config.log.level || 'info'
   }
+
+  const config = loadConfig(file)
+  const keys = Object.keys(defaultConf)
+
+  for (let key of keys) {
+    config[key] = config[key] || defaultConf[key]
+    for (let p in defaultConf[key]) {
+      if (undefined === config[key][p]) config[key][p] = defaultConf[key][p]
+    }
+  }
+
+  // enable undefined modules
+  for (let module in defaultConf.api.modules) {
+    config.api.modules[module] = config.api.modules[module] !== false
+  }
+
+  // defaults  servers/ports
+
+  config.blocks.node = config.blocks.node || config.source.node
+  config.blocks.port = config.blocks.port || config.source.port
+
+  let s = config.source
+  config.source.url = config.source.url || `${s.protocol}://${s.node}:${s.port}`
+
+  // defaults log files
+
+  defaultLogs('api')
+  defaultLogs('blocks')
+
+  config.api.collectionsNames = config.collectionsNames
+  return config
 }
 
-// enable undefined modules
-for (let module in defaultConf.api.modules) {
-  config.api.modules[module] = config.api.modules[module] !== false
-}
-
-// defaults  servers/ports
-
-config.blocks.node = config.blocks.node || config.source.node
-config.blocks.port = config.blocks.port || config.source.port
-
-let s = config.source
-config.source.url = config.source.url || `${s.protocol}://${s.node}:${s.port}`
-
-// defaults log files
-
-defaultLogs('api')
-defaultLogs('blocks')
-
-config.api.collectionsNames = config.collectionsNames
-
-function defaultLogs (key) {
-  const dir = config.log.dir
-  if (!dir) return
-  config[key].log = config[key].log || {}
-  config[key].log.file = config[key].log.file || `${dir}/${key}.json`
-  config[key].log.level = config[key].log.level || config.log.level || 'info'
-}
-
-function loadConfig () {
+function loadConfig (file) {
   let config = {}
-  try {
-    let file = path.resolve(__dirname, '../../config.json')
-    if (fs.existsSync(file)) config = JSON.parse(fs.readFileSync(file, 'utf-8'))
-  } catch (err) {
-    console.log(err)
-    process.exit(8)
+  if (file) {
+    try {
+      file = path.resolve(__dirname, file)
+      if (fs.existsSync(file)) config = JSON.parse(fs.readFileSync(file, 'utf-8'))
+    } catch (err) {
+      console.log(err)
+      process.exit(8)
+    }
   }
   return config
 }
