901e44f0684a23166ad9d52ee4205d3b14b9839c
emi <emiliorizzo@gmail.com>
2019-12-23 20:02:25 -0300

    Change defaultConfig, rename stats collection

--- src/lib/defaultConfig.js
+++ src/lib/defaultConfig.js
@@ -59,7 +59,7 @@ export default {
     OrphanBlocks: 'orphanBlocks',
     TxPool: 'txPool',
     PendingTxs: 'transactionsPending',
-    Stats: 'stats',
+    Stats: 'statsCollection',
     BlocksSummary: 'blocksSummary',
     ContractVerification: 'contractsVerifications',
     VerificationsResults: 'verificationResults'
