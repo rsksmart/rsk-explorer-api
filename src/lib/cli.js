74012359e27621d2b86137edbfab3331de640082
emi <emiliorizzo@gmail.com>
2020-01-11 14:19:20 +0100

    Update tools/wsGet, add file stream

--- src/lib/cli.js
+++ src/lib/cli.js
@@ -3,11 +3,13 @@ export const red = '\x1b[31m'
 export const blue = '\x1b[36m'
 export const green = '\x1b[32m'
 export const orange = '\x1b[33m'
+export const grey = '\x1b[90m'
 
 export const error = l => console.log(red, l, reset)
 export const warn = l => console.log(orange, l, reset)
 export const info = l => console.log(blue, l, reset)
 export const ok = l => console.log(green, l, reset)
+export const example = l => console.log(grey, l, reset)
 
 export const ansiCode = number => `\x1b[${parseInt(number)}m`
 
--- src/tools/wsGet.js
+++ src/tools/wsGet.js
@@ -1,23 +1,26 @@
 import io from 'socket.io-client'
 import * as c from '../lib/cli'
 import fs from 'fs'
-import util from 'util'
+import crypto from 'crypto'
+import * as URL from 'url'
+import pkg from '../../package.json'
 
-const writeFile = util.promisify(fs.writeFile)
-const url = process.env.url || 'http://localhost:3003'
+const url = process.env.url || 'ws://localhost:3003'
+if (process.argv[2] === '--help') help()
 const outDir = process.env.outDir || '/tmp'
 
 let payload = process.env.payload || process.argv[2]
 
-if (!url || !payload) help()
+if (!isValidURL(url) || !payload) help()
 payload = JSON.parse(payload)
 if (!payload.module || !payload.action || !payload.params) help()
 
-const outFile = `${outDir}/${payload.module}-${payload.action}.json`
+const destinationFile = getDestinationFile(payload)
+const file = createFileStream(destinationFile)
 
 const socket = io.connect(url, { reconnect: true })
-let results = []
-const key = `${payload.module}${payload.action}${Date.now()}${Math.round(Math.random())}`
+let results = 0
+const key = createRequestKey(payload)
 payload.key = key
 
 c.info(`Waiting for WS on ${url}`)
@@ -47,22 +50,20 @@ socket.on('data', async res => {
 
         c.info(`Adding ${data.length}`)
 
-        if (Array.isArray(data)) results = results.concat(data)
-        else results.push(data)
+        // send data to file stream
+        await addDataToFile(data)
 
         if (!payload.limit) payload.limit = limit
 
         if (next) {
           getPage(socket, payload, next)
         } else {
-          c.ok(`Done: ${results.length} results`)
-          if (results.length) await saveToFile(results, outFile)
-          process.exit(0)
+          await closeFileAndExit()
         }
       } else { // single result
         c.ok('Saving to file')
-        await saveToFile(data, outFile)
-        process.exit(0)
+        await addDataToFile(data)
+        await closeFileAndExit()
       }
     }
   } catch (err) {
@@ -82,12 +83,45 @@ process.on('unhandledRejection', err => {
   process.exit(9)
 })
 
+function getDestinationFile (payload, count = 0) {
+  count = count || 0
+  let { module, action } = payload
+  let suffix = (count) ? `-${count}` : ''
+  let fileName = `${module}-${action}${suffix}.json`
+  let dest = `${outDir}/${fileName}`
+  if (fs.existsSync(dest)) return getDestinationFile(payload, count + 1)
+  return dest
+}
+
+async function addDataToFile (data) {
+  try {
+    let items = await file.put(data)
+    results += items
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
+
+async function closeFileAndExit () {
+  try {
+    await file.close()
+    c.ok(`Done: ${results} results`)
+    c.info(`File saved: ${destinationFile}`)
+    process.exit(0)
+  } catch (err) {
+    c.error(err)
+    process.exit(9)
+  }
+}
+
 function getPage (socket, payload, next) {
-  let { limit } = payload
+  let params = payload.params || {}
+  let { limit } = params
   let count = false
   limit = limit || ''
   if (!next) {
     count = true
+    if (params.next) next = params.next
     c.ok(`Getting first ${limit} items`)
   } else {
     c.ok(`Getting next ${limit} items: ${next}`)
@@ -100,18 +134,83 @@ function getPage (socket, payload, next) {
 }
 
 function help () {
-  if (!url) c.info('Set enviroment variable url=[explorer-api-url]')
-  if (!payload) c.info(`Set enviroment variable payload, e.g. payload='{"module":"blocks","action":"getBlock","params":{"hashOrNumber":200}}'`)
-  c.info(`Usage: payload=[payload] url=[url] [outDir=(path)] | ${process.argv[0]} ${process.argv[1]} { payload }`)
+  let { name } = pkg
+  if (!isValidURL(url)) c.warn(`Invalid URL: ${url}`)
+  // if (!payload) c.warn(`Set environment variable payload, e.g. payload='{"module":"blocks","action":"getBlock","params":{"hashOrNumber":200}}'`)
+  c.ok('')
+  c.ok(`Usage:`)
+  c.info('')
+  c.info('All parameters must be provided as environment variables')
+
+  c.info('')
+  c.info('Required parameters:')
+  c.info('')
+  c.example(`     url: ${name} instance URL`)
+  c.example(`     payload: ${name} payload`)
+  c.info('')
+  c.info('Optionals parameters:')
+  c.info('')
+  c.example(`     outDir: destination folder`)
+  c.info('')
+  c.ok('Examples:')
+  c.example('')
+  c.info('Get block')
+  c.example(`    export url=wss://backend.explorer.rsk.co`)
+  c.example(`    export payload='{"module":"blocks","action":"getBlock","params":{"hashOrNumber":200}}'`)
+  c.example('')
+  c.info('Get blocks')
+  c.example(`    export url=wss://backend.explorer.rsk.co`)
+  c.example(`    export payload='{"module":"blocks","action":"getBlocks","params":{"next":200,"sort":{"number":-1}}}'`)
+  c.example('')
   process.exit(0)
 }
 
-async function saveToFile (data, file) {
+function createFileStream (destinationFile) {
+  const file = fs.createWriteStream(destinationFile)
+  const addLineToFile = (data) => {
+    return new Promise((resolve, reject) => {
+      let line = JSON.stringify(data) + '\n'
+      const errorListener = () => reject(new Error('Error adding line to file'))
+      // use addListener instead of 'on' to remove event later, and prevent memory leaks
+      file.addListener('error', errorListener)
+      file.write(line, () => resolve())
+      file.removeListener('error', errorListener)
+    })
+  }
+
+  const put = async (data) => {
+    try {
+      if (!Array.isArray(data)) data = [data]
+      for (let d of data) {
+        await addLineToFile(d)
+      }
+      return data.length
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  }
+  const close = () => {
+    return new Promise((resolve, reject) => {
+      file.end(() => resolve())
+      file.on('error', err => {
+        reject(err)
+      })
+    })
+  }
+  return Object.freeze({ put, close })
+}
+
+function createRequestKey ({ action, module }) {
+  let rnd = crypto.randomBytes(8).toString('hex')
+  return `${module} - ${action} - ${rnd}`
+}
+
+function isValidURL (url) {
   try {
-    await writeFile(file, JSON.stringify(data))
-    c.ok(`File saved: ${file}`)
+    let { protocol } = URL.parse(url)
+    return /^ws/.test(protocol)
   } catch (err) {
-    console.error(`Error writing file ${file}: ${err}`)
-    process.exit(7)
+    c.error(err)
+    return false
   }
 }
