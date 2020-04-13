dde3c9b101a5d68ae6ea536e10ddcda2e68d3f1c
emi <emiliorizzo@gmail.com>
2020-04-13 17:00:32 -0600

    Update Tx

--- src/services/classes/Tx.js
+++ src/services/classes/Tx.js
@@ -1,45 +1,67 @@
 import { BcThing } from './BcThing'
 import { formatEvent } from './Event'
-import ContractParser from 'rsk-contract-parser'
 import { txTypes } from '../../lib/types'
 import { getTxOrEventId } from '../../lib/ids'
-
+import ContractParser from 'rsk-contract-parser'
 import TxTrace from './TxTrace'
 import { Addresses } from './Addresses'
+
 export class Tx extends BcThing {
-  constructor (hash, timestamp, { addresses, txData, blockTrace, traceData, nod3, initConfig, collections, notTrace } = {}) {
+  constructor (hash, timestamp, { addresses, txData, blockTrace, traceData, nod3, initConfig, collections, notTrace, receipt } = {}) {
     if (!hash || !timestamp) throw new Error(`Tx, missing arguments`)
     super({ nod3, initConfig, collections })
     if (!this.isTxOrBlockHash(hash)) throw new Error(`Tx, ${hash} is not a tx hash`)
     this.hash = hash
     this.timestamp = timestamp
     this.txData = txData
+    this.receipt = receipt
     this.toAddress = undefined
-    if (blockTrace) traceData = this.getTraceDataFromBlock(blockTrace)
-    this.addresses = addresses || new Addresses({ nod3, initConfig, collections })
+    if (blockTrace) traceData = getTraceDataFromBlock(hash, blockTrace)
+    addresses = addresses || new Addresses({ nod3, initConfig, collections })
+    this.addresses = addresses
     this.trace = (!notTrace) ? new TxTrace(hash, { traceData, nod3, initConfig }) : undefined
+    this.data = {
+      tx: {},
+      events: [],
+      tokenAddresses: []
+    }
   }
-  getTraceDataFromBlock (blockTrace) {
-    if (!Array.isArray(blockTrace)) return
-    let { hash } = this
-    let key = blockTrace.findIndex(v => Array.isArray(v) && v[0].transactionHash === hash)
-    return blockTrace[key]
-  }
-  async fetch () {
+  async fetch (force) {
     try {
+      let { fetched } = this
+      if (fetched && !force) return this.getData()
       let tx = await this.getTx()
+      if (!tx) throw new Error('Error getting tx')
       await this.setToAddress(tx)
+      this.addresses.add(tx.from)
+      let { contractAddress } = tx.receipt
+      if (contractAddress) this.addresses.add(contractAddress)
       tx = this.txFormat(tx)
-      let data = { tx }
-      data.events = await this.parseEvents(tx)
+      let { nod3, initConfig } = this
+      let { contract, isNative } = this.toAddress || {}
+      let parser
+      if (contract) parser = await contract.getParser()
+      if (isNative) parser = new ContractParser({ nod3, initConfig })
+      if (parser) {
+        tx.receipt.logs = parseLogs(tx, parser)
+        let events = parseEvents(tx, parser)
+        let eventAddresses = [].concat(...events.filter(e => e._addresses).map(({ _addresses }) => _addresses))
+        eventAddresses.forEach(address => this.addresses.add(address))
+        this.setData({ events })
+        if (contract) {
+          eventAddresses.forEach(address => contract.addAddress(address))
+          let tokenAddresses = await contract.fetchAddresses()
+          this.setData({ tokenAddresses })
+        }
+      }
       if (this.trace) {
         let trace = await this.trace.fetch()
-        let { internalTxs, addresses } = await this.trace.getInternalTransactionsData(trace)
+        let { internalTransactions, addresses } = await this.trace.getInternalTransactionsData(trace)
         addresses.forEach(address => this.addresses.add(address))
-        data.internalTxs = internalTxs
-        data.trace = trace
+        this.setData({ trace, internalTransactions })
       }
-      this.data = data
+      this.setData({ tx })
+      this.fetched = true
       return this.getData()
     } catch (err) {
       return Promise.reject(err)
@@ -47,25 +69,39 @@ export class Tx extends BcThing {
   }
 
   async setToAddress ({ to }) {
-    this.toAddress = to
-    if (this.isAddress(to)) {
-      this.toAddress = this.addresses.add(to)
-      await this.toAddress.fetch()
+    try {
+      let { isAddress, toAddress } = this
+      if (toAddress !== undefined) return toAddress
+      if (to !== null) {
+        if (!isAddress(to)) throw new Error(`Invalid address ${to}`)
+        this.toAddress = this.addresses.add(to)
+        await this.toAddress.fetch()
+      } else {
+        this.toAddress = null
+      }
+      return this.toAddress
+    } catch (err) {
+      return Promise.reject(err)
     }
   }
 
   async getTx () {
     try {
       let txHash = this.hash
-      let tx = this.txData
-      if (!this.isTxData(tx)) tx = await this.getTransactionByHash(txHash)
-      if (tx.hash !== txHash) throw new Error(`Error getting tx: ${txHash}, hash received:${tx.hash}`)
-      let receipt = await this.getTxReceipt(txHash)
-      if (!receipt) throw new Error(`The Tx ${txHash} .receipt is: ${receipt} `)
+      let { txData } = this
+      if (!this.isTxData(txData)) {
+        txData = await this.getTransactionByHash(txHash)
+        this.txData = txData
+      }
+      if (txData.hash !== txHash) throw new Error(`Error getting tx: ${txHash}, hash received:${txData.hash}`)
+      let { receipt } = this
+      if (!receipt) {
+        receipt = await this.getTxReceipt(txHash)
+        if (!receipt) throw new Error(`The Tx ${txHash} .receipt is: ${receipt} `)
+      }
+      this.receipt = receipt
       let { timestamp } = this
-      tx = createTxObject(tx, { timestamp, receipt })
-      let { contractAddress } = receipt
-      if (contractAddress) this.addresses.add(contractAddress)
+      let tx = createTxObject(txData, { timestamp, receipt })
       if (!tx.transactionIndex) tx.transactionIndex = receipt.transactionIndex
       return tx
     } catch (err) {
@@ -80,19 +116,7 @@ export class Tx extends BcThing {
   getTxReceipt (txHash) {
     return this.nod3.eth.getTransactionReceipt(txHash)
   }
-  async parseEvents (tx) {
-    try {
-      let logs = await this.parseLogs(tx.receipt.logs)
-      return logs.map(l => {
-        l = formatEvent(l, tx)
-        let event = Object.assign({}, l)
-        delete l._id
-        return event
-      })
-    } catch (err) {
-      return Promise.reject(err)
-    }
-  }
+
   txFormat (tx) {
     let type = txTypes.default
     const receipt = tx.receipt || {}
@@ -107,13 +131,6 @@ export class Tx extends BcThing {
     return tx
   }
 
-  parseLogs (logs) {
-    const { nod3, initConfig } = this
-    let parser = new ContractParser({ initConfig, nod3 })
-    return new Promise((resolve, reject) => {
-      process.nextTick(() => resolve(parser.parseTxLogs(logs)))
-    })
-  }
   isTxData (data) {
     if (!data || typeof data !== 'object') return
     return data.hash && data.blockHash && data.input
@@ -129,4 +146,23 @@ export function createTxObject (tx, { timestamp, receipt }) {
   return tx
 }
 
+export function getTraceDataFromBlock (hash, blockTrace) {
+  if (!Array.isArray(blockTrace)) return
+  let key = blockTrace.findIndex(v => Array.isArray(v) && v[0].transactionHash === hash)
+  return blockTrace[key]
+}
+
+export function parseLogs (tx, parser) {
+  return parser.parseTxLogs(tx.receipt.logs)
+}
+export function parseEvents (tx, parser) {
+  return tx.receipt.logs.map(l => {
+    l = formatEvent(l, tx)
+    let event = Object.assign({}, l)
+    delete l._id
+    return event
+  })
+}
+
+export { txTypes }
 export default Tx
--- test/services/Tx.spec.js
+++ test/services/Tx.spec.js
@@ -1,8 +1,9 @@
-import Tx from '../../src/services/classes/Tx'
+import { Tx, txTypes } from '../../src/services/classes/Tx'
 import blocks from './blockData'
 import { expect } from 'chai'
 import { BlocksBase } from '../../src/lib/BlocksBase'
 import datasource from '../../src/lib/dataSource'
+import Address from '../../src/services/classes/Address'
 
 describe(`#Tx`, function () {
   for (let blockData of blocks) {
@@ -18,8 +19,13 @@ describe(`#Tx`, function () {
           let tx = new Tx(hash, timestamp, new BlocksBase(db, { initConfig }))
           await tx.fetch()
           let data = await tx.getData()
+          expect(tx.toAddress instanceof Address).to.be.equal(true, 'toAddress must be an instance of Address')
+          let { isContract, isNative } = tx.toAddress
+          expect(typeof isContract).to.be.equal('function', 'toAddress.isContract must be a function')
+          expect(typeof isNative).to.be.equal('boolean', 'isNative must be a boolean variable')
+          let expectedType = (tx.toAddress.isContract() && !isNative) ? txTypes.call : txe.txType
           expect(data).to.haveOwnProperty('tx')
-          expect(data.tx.txType).to.be.deep.equal(txe.txType, 'tx type')
+          expect(data.tx.txType).to.be.deep.equal(expectedType, 'tx type')
           for (let p in txe.receipt) {
             expect(data.tx.receipt[p]).to.be.deep.equal(txe.receipt[p], p)
           }
