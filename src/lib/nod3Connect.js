9e4e2e56f3e0bcb029addf24f2782b74294d3695
emi <emiliorizzo@gmail.com>
2020-05-23 03:07:16 -0600

    Update nod3Connect, add nod3Hub

--- src/lib/nod3Connect.js
+++ src/lib/nod3Connect.js
@@ -1,13 +1,29 @@
-import Nod3 from 'nod3'
+import { Nod3, Nod3Hub } from 'nod3'
 import config from './config'
-const url = config.source.url
 
-export const nod3Connect = (url) => {
-  return new Nod3(
-    new Nod3.providers.HttpProvider(url)
-  )
+const { HttpProvider } = Nod3.providers
+const { source } = config
+
+export const nod3Connect = (url, options = {}) => new Nod3(new HttpProvider(url), options)
+
+// return nod3 | nod3Hub based on source
+export const nod3BySource = (source, options = {}) => {
+  if (Array.isArray(source)) return createNod3Hub(source, options)
+  return nod3Instance(source, options)
 }
 
-export const nod3 = nod3Connect(url)
+export const createNod3Hub = (sources, options) => {
+  const providers = sources.map(({ url }) => new HttpProvider(url))
+  return Nod3Hub(providers, options)
+}
+
+// Returns always a nod3 instance
+export const nod3Instance = (source, options = {}) => {
+  const sources = (!Array.isArray(source)) ? [source] : [...source]
+  return nod3Connect(sources[0].url, options)
+}
+
+export const nod3 = nod3Instance(source)
+export const nod3Hub = nod3BySource(source)
 
 export default nod3
