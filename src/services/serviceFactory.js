db716f7dbdbba701b26855caecdc05766d5813f5
emi <emiliorizzo@gmail.com>
2020-06-30 15:19:47 -0600

    Update serviceFactory, add createRouter

--- src/services/serviceFactory.js
+++ src/services/serviceFactory.js
@@ -2,6 +2,7 @@ import config from '../lib/config'
 import { Logger } from '../lib/Logger'
 import { createPorts, createServices } from './servicesConfig'
 import { Service } from './Service/ServiceServer'
+import { Router } from './Router'
 
 const { blocks } = config
 const { address } = blocks
@@ -17,19 +18,48 @@ export const createServiceLogger = ({ name, uri }) => {
 
 export async function createService (serviceConfig, executor, { log } = {}) {
   try {
-    const { uri, name, address } = serviceConfig
+    const { uri, name, address, port } = serviceConfig
     log = log || createServiceLogger(serviceConfig)
     const service = Service(uri, { name }, executor)
-    const startService = async () => {
-      try {
-        let port = await service.start()
-        if (port !== serviceConfig.port) throw new Error('Binding port mismatch')
-        log.info(`Service ${name} listening on ${address}:${port}`)
-      } catch (err) {
-        return Promise.reject(err)
+    const startService = createStartService(service, { name, address, port }, { log })
+    return { service, log, startService }
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
+
+export function createStartService (service, { name, address, port }, { log }) {
+  return async () => {
+    try {
+      let listenPort = await service.start()
+      if (listenPort !== port) throw new Error('Binding port mismatch')
+      if (log) log.info(`Service ${name} listening on ${address}:${port}`)
+      return listenPort
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  }
+}
+
+export async function createRouter (routerServiceConfig, { services, log }) {
+  try {
+    services = services || {}
+    log = log || createServiceLogger(routerServiceConfig)
+    const router = Router({ log })
+    const executor = ({ create }) => {
+      create.Emitter()
+      create.Listener(router.broadcast)
+    }
+    const { service, startService } = await createService(routerServiceConfig, executor, { log })
+    for (let s in services) {
+      let config = services[s]
+      let { name } = config
+      if (config.uri !== routerServiceConfig.uri) {
+        router.addService(name, config)
       }
     }
-    return { service, log, startService }
+    router.setRouterService(service)
+    return Object.freeze({ router, startService, log })
   } catch (err) {
     return Promise.reject(err)
   }
