6bfbf5dda28433582ea3aa888a8a6fb49b5ccf7c
emi <emiliorizzo@gmail.com>
2020-07-08 12:13:25 -0600

    Test, update storedConfig test

--- src/lib/StoredConfig.js
+++ src/lib/StoredConfig.js
@@ -2,6 +2,8 @@
 import config from './config'
 const collectionName = config.collectionsNames.Config
 
+export const readOnlyError = id => `The doc with _id ${id} is read only`
+
 export function StoredConfig (db, readOnlyDocsIds = []) {
   const storage = db.collection(collectionName)
   const isReadOnly = _id => readOnlyDocsIds.includes(_id)
@@ -23,6 +25,10 @@ export function StoredConfig (db, readOnlyDocsIds = []) {
   const save = async (id, doc) => {
     try {
       if (!id || !isValidId(id)) throw new Error(`Invalid id ${id}`)
+      if (isReadOnly(id)) {
+        let exists = await get(id)
+        if (exists) throw new Error(readOnlyError(id))
+      }
       const newDoc = Object.assign({}, doc)
       newDoc._id = id
       newDoc._created = Date.now()
@@ -35,15 +41,16 @@ export function StoredConfig (db, readOnlyDocsIds = []) {
   const update = async (_id, doc, { create } = {}) => {
     try {
       if (!_id) throw new Error(`Missing doc._id`)
-      if (isReadOnly(_id)) throw new Error(`The doc with _id ${_id} is read only`)
+      if (isReadOnly(_id)) throw new Error(readOnlyError(_id))
       const newDoc = Object.assign({}, doc)
-      newDoc._updated = Date.now()
+
       const options = {}
       if (create) {
         let old = await get(_id)
-        if (!old) return save(_id, doc)
+        if (!old) return save(_id, newDoc)
       }
-      let res = await storage.updateOne({ _id }, { $set: doc }, options)
+      newDoc._updated = Date.now()
+      let res = await storage.updateOne({ _id }, { $set: newDoc }, options)
       return res
     } catch (err) {
       return Promise.reject(err)
