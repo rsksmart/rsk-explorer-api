976225079834f4fda2d48934c32bf616afe17c19
emi <emiliorizzo@gmail.com>
2020-07-10 00:54:39 -0600

    Update API.Balances, add getBalance

--- src/api/modules/Balances.js
+++ src/api/modules/Balances.js
@@ -1,4 +1,5 @@
 import { DataCollectorItem } from '../lib/DataCollector'
+import { isBlockHash } from '../../lib/utils'
 
 export class Balances extends DataCollectorItem {
   constructor ({ Balances }, name) {
@@ -6,6 +7,47 @@ export class Balances extends DataCollectorItem {
     super(Balances, name, { sortDir: -1, sortable })
     this.fields = {}
     this.publicActions = {
+      /**
+       * @swagger
+       * /api?module=balances&action=getBalance:
+       *    get:
+       *      description: get address balance at blockNumber (if exists)
+       *      tags:
+       *        - balances
+       *      parameters:
+       *        - name: module
+       *          in: query
+       *          required: true
+       *          enum: [balances]
+       *        - name: action
+       *          in: query
+       *          required: true
+       *          enum: [getBalance]
+       *        - $ref: '#/parameters/address'
+       *        - name: block
+       *          in: query
+       *          required: true
+       *          schema:
+       *            type: string
+       *            description: block hash or block number
+       *            example: 30000
+       *      responses:
+       *        200:
+       *          $ref: '#/definitions/Response'
+       *        400:
+       *          $ref: '#/responses/BadRequest'
+       *        404:
+       *          $ref: '#/responses/NotFound'
+      */
+      getBalance: async params => {
+        const { address, block } = params
+        const query = { address }
+
+        if (isBlockHash(block)) query.blockHash = block
+        else query.blockNumber = parseInt(block)
+
+        return this.getItem(query, params)
+      },
       /**
        * @swagger
        * /api?module=balances&action=getBalances:
