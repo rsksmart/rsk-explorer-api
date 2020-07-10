335a7a975c840c0ddb9295f167db46e978523589
emi <emiliorizzo@gmail.com>
2020-07-10 03:21:37 -0600

    Update API.Balances add getStatus

--- src/api/modules/Balances.js
+++ src/api/modules/Balances.js
@@ -76,6 +76,36 @@ export class Balances extends DataCollectorItem {
       getBalances: async params => {
         const { address } = params
         return this.getPageData({ address }, params)
+      },
+      /**
+       * @swagger
+       * /api?module=balances&action=getStatus:
+       *    get:
+       *      description: get status of balances
+       *      tags:
+       *        - balances
+       *      parameters:
+       *        - name: module
+       *          in: query
+       *          required: true
+       *          enum: [balances]
+       *        - name: action
+       *          in: query
+       *          required: true
+       *          enum: [getStatus]
+       *      responses:
+       *        200:
+       *          $ref: '#/definitions/Response'
+       *        400:
+       *          $ref: '#/responses/BadRequest'
+       *        404:
+       *          $ref: '#/responses/NotFound'
+      */
+      getStatus: async params => {
+        const projection = { blockHash: 1, blockNumber: 1, _id: 0 }
+        const fromBlock = await this.getOne({}, projection, { blockNumber: 1 })
+        const toBlock = await this.getOne({}, projection, { blockNumber: -1 })
+        return { data: { fromBlock, toBlock } }
       }
     }
   }
