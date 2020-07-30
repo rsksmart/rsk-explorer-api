f0f0824c90644e42de94eb159a810e7a7cb15c8c
emi <emiliorizzo@gmail.com>
2020-07-30 13:18:57 -0600

    Update Event, set _addresses default value

--- src/services/classes/Event.js
+++ src/services/classes/Event.js
@@ -1,11 +1,13 @@
 import { getEventId } from '../../lib/ids'
 
 export const formatEvent = (event, tx) => {
-  let { timestamp, receipt } = tx
-  let id = getEventId(event, tx)
+  if (!event) return
+  const { timestamp, receipt } = tx
+  const id = getEventId(event)
   event.eventId = id
   event.timestamp = timestamp
   event.txStatus = receipt.status
   event.event = event.event || null
+  event._addresses = event._addresses || []
   return event
 }
