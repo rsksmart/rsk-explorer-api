83cc27ac0d1e3df2a82faeee9ea1ee8a7b6f8e73
emi <emiliorizzo@gmail.com>
2021-01-29 15:22:09 -0700

    Update API, allow POST in /api routes

--- src/api/routes/api.js
+++ src/api/routes/api.js
@@ -1,39 +1,47 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 const router = express.Router()
 
 const Routes = ({ log, api }) => {
-  router.use('/', (req, res, next) => {
+  const getResult = async (api, { module, action, params }) => {
     try {
-      const params = req.query
-      if (Object.keys(params).length === 0) {
-        res.send(api.info())
-      } else {
-        const { module, action } = req.query
-        if (!module) throw new Error(`invalid module: ${module}`)
-        if (!action) throw new Error(`invalid action: ${action}`)
-        next()
-      }
-    } catch (err) {
-      log.debug(err)
-      res.status(400).send()
-    }
-  })
-
-  router.get('/', async (req, res, next) => {
-    try {
-      const params = req.query
-      const { module, action } = req.query
-      delete params.module
-      delete params.action
       const { result } = await api.run({ module, action, params })
       if (!result) throw new Error('Missing result')
       if (!result.data) throw new Error('Missing data')
+      return result
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  }
+  const sendResult = async (api, res, { module, action, params }) => {
+    let result
+    try {
+      if (!!module !== !!action) {
+        res.status(400).send()
+        return
+      }
+      if (!module && !action) result = api.info()
+      else result = await getResult(api, { module, action, params })
+      if (!result) throw new Error('Empty result')
       res.send(result)
     } catch (err) {
       res.status(404).send()
       log.error(err)
     }
+  }
+
+  router.get('/', (req, res, next) => {
+    const params = req.query
+    const { module, action } = params
+    delete params.module
+    delete params.action
+    return sendResult(api, res, { module, action, params })
   })
+
+  router.post('/', bodyParser.json(), (req, res, next) => {
+    return sendResult(api, res, req.body)
+  })
+
   return router
 }
 
