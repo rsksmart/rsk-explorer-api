ee54cf286e47a98356cda249d2abf856c9f3064b
emi <emiliorizzo@gmail.com>
2021-02-01 17:01:43 -0300

    Update routes/api, add send CB

--- src/api/routes/api.js
+++ src/api/routes/api.js
@@ -2,8 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 const router = express.Router()
 
-const Routes = ({ log, api }) => {
-  const getResult = async (api, { module, action, params }) => {
+const Routes = ({ log, api }, send) => {
+  const getResult = async ({ module, action, params }) => {
     try {
       const { result } = await api.run({ module, action, params })
       if (!result) throw new Error('Missing result')
@@ -13,7 +13,8 @@ const Routes = ({ log, api }) => {
       return Promise.reject(err)
     }
   }
-  const sendResult = async (api, res, { module, action, params }) => {
+  const sendResult = async ({ res, req, next }, payload) => {
+    const { action } = payload
     let result
     try {
       if (!!module !== !!action) {
@@ -21,9 +22,10 @@ const Routes = ({ log, api }) => {
         return
       }
       if (!module && !action) result = api.info()
-      else result = await getResult(api, { module, action, params })
+      else result = await getResult(payload)
       if (!result) throw new Error('Empty result')
-      res.send(result)
+      if (typeof send === 'function') send({ result, res, req, next, payload })
+      else res.send(result)
     } catch (err) {
       res.status(404).send()
       log.error(err)
@@ -35,11 +37,11 @@ const Routes = ({ log, api }) => {
     const { module, action } = params
     delete params.module
     delete params.action
-    return sendResult(api, res, { module, action, params })
+    return sendResult({ res, req, next }, { module, action, params })
   })
 
   router.post('/', bodyParser.json(), (req, res, next) => {
-    return sendResult(api, res, req.body)
+    return sendResult({ res, req, next }, req.body)
   })
 
   return router
