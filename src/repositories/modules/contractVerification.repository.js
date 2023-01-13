45ab42df153639a07c87b7593b5be7798de8f786
Dario <dario.gomez@iovlabs.org>
2023-01-13 09:18:41 -0300

    feat: added contractVerification module repository

---
+++ src/repositories/modules/contractVerification.repository.js
@@ -0,0 +1,14 @@
+export const contractVerificationRepository = {
+  async findOne (query = {}, options = {}, collection) {
+    return collection.findOne(query, options)
+  },
+  async find (query = {}, options = {}, collection) {
+    return collection.find(query, options)
+  },
+  async countDocuments (query = {}, collection) {
+    return collection.countDocuments(query)
+  },
+  async aggregate (aggregate, collection) {
+    return collection.aggregate(aggregate)
+  }
+}
