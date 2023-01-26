9f18930e482ba23be661e640d3344e42d64a7104
antoniolottero <antonio.dante@iovlabs.org>
2023-01-26 10:03:49 -0300

    feat: added balancesLog repository

---
+++ src/repositories/balancesLog.repository.js
@@ -0,0 +1,21 @@
+export const balancesLogRepository = {
+  async findOne (query = {}, options = {}, collection) {
+    return collection.findOne(query, options)
+  },
+  async find (query = {}, options = {}, collection, sort = {}, limit = {}) {
+    return collection
+      .find(query, options)
+      .sort(sort)
+      .limit(limit)
+      .toArray()
+  },
+  async countDocuments (query = {}, collection) {
+    return collection.countDocuments(query)
+  },
+  async aggregate (aggregate, collection) {
+    return collection.aggregate(aggregate).toArray()
+  },
+  async insertOne (data, collection) {
+    return collection.insertOne(data)
+  }
+}
