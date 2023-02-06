4713bc52187684db771847f1e362dbb54332c4f1
Dario <dario.gomez@iovlabs.org>
2023-02-06 16:04:43 -0300

    feat: implemented insertOne method and its converter utils

---
+++ src/converters/abi.converters.js
@@ -0,0 +1,25 @@
+function rawAbiToEntity (data) {
+  return {
+    anonymous: data.anonymous,
+    name: data.name,
+    type: data.type
+  }
+}
+
+function rawInputToEntity (data) {
+  return {
+    name: data.name,
+    type: data.type,
+    indexed: data.indexed
+  }
+}
+
+function rawAbiInputToEntity (data) {
+  return {
+    abi_id: data.abiId,
+    name: data.name,
+    type: data.type
+  }
+}
+
+export {rawAbiToEntity, rawInputToEntity, rawAbiInputToEntity}
--- src/repositories/tx.repository.js
+++ src/repositories/tx.repository.js
@@ -1,3 +1,7 @@
+import {prismaClient} from '../lib/Setup'
+import {rawTxToEntity, rawReceiptToEntity, rawLogToEntity, rawLogTopicToEntity, rawLogArgToEntity, rawLoggedAddressToEntity} from '../converters/tx.converters'
+import {rawAbiToEntity, rawInputToEntity, rawAbiInputToEntity} from '../converters/abi.converters'
+
 export const txRepository = {
   findOne (query = {}, project = {}, collection) {
     return collection.findOne(query, project)
@@ -25,7 +29,65 @@ export const txRepository = {
   deleteMany (filter, collection) {
     return collection.deleteMany(filter)
   },
-  insertOne (data, collection) {
-    return collection.insertOne(data)
+  async insertOne (data, collection) {
+    try {
+      const thisType = {
+        type: data.txType,
+        entity: 'transaction'
+      }
+      let existingType = await prismaClient.type.findFirst({where: thisType})
+      if (!existingType) {
+        existingType = await prismaClient.type.create({data: thisType})
+      }
+      await prismaClient.transaction.create({data: rawTxToEntity({txTypeId: existingType.id, ...data})})
+
+      await prismaClient.receipt.create({data: rawReceiptToEntity(data.receipt)})
+
+      const {logs} = data.receipt
+
+      for (const log of logs) {
+        const {abi, topics, args, transactionHash, logIndex, _addresses} = log
+
+        if (abi) {
+          const {inputs} = abi
+          const abiToSave = rawAbiToEntity(abi)
+          let existingAbi = await prismaClient.abi.findFirst({where: abiToSave})
+          if (!existingAbi) {
+            existingAbi = await prismaClient.abi.create({data: abiToSave})
+          }
+
+          if (inputs) {
+            for (const input of inputs) {
+              let existingInput = await prismaClient.input.findFirst({where: {name: input.name, type: input.type}})
+              if (!existingInput) {
+                existingInput = await prismaClient.input.create({data: rawInputToEntity(input)})
+              }
+              await prismaClient.abi_input.create({data: rawAbiInputToEntity({abiId: existingAbi.id, ...existingInput})})
+            }
+          }
+        }
+
+        await prismaClient.log.create({data: rawLogToEntity(log)})
+
+        for (const topic of topics) {
+          await prismaClient.log_topic.create({data: rawLogTopicToEntity({topic, transactionHash, logIndex})})
+        }
+
+        if (args) {
+          for (const arg of args) {
+            await prismaClient.log_arg.create({data: rawLogArgToEntity({arg, transactionHash, logIndex})})
+          }
+        }
+
+        for (const address of _addresses) {
+          await prismaClient.logged_address.create({data: rawLoggedAddressToEntity({address, transactionHash, logIndex})})
+        }
+      }
+    } catch (e) {
+      console.log(e)
+    }
+    const mongoRes = await collection.insertOne(data)
+
+    return mongoRes
   }
 }
