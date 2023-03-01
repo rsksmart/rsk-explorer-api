1e2e4d987bff5a667682f4a7c5c0b00f993fbcbf
antoniolottero <antonio.dante@iovlabs.org>
2023-03-01 16:43:03 -0300

    feat: added to balancesLogRepository prisma insertOne and findOne, eliminated unused functions.

---
+++ src/converters/balancesLog.converters.js
@@ -0,0 +1,15 @@
+function rawBalancesLogToEntity ({_created, blockHash}) {
+  return {
+    created: String(_created),
+    blockHash: blockHash
+  }
+}
+
+function entityToRawBalancesLog (data) {
+  if (data) {
+    data._created = Number(data.created)
+    delete data.created
+  }
+  return data
+}
+export { rawBalancesLogToEntity, entityToRawBalancesLog }
