f7d79e91521612dab915a66a45fde335eeef2dd9
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-03-02 19:17:40 -0300

    feat: replaced mongo for prisma crud methods in configRepository

--- src/repositories/config.repository.js
+++ src/repositories/config.repository.js
@@ -1,31 +1,107 @@
+import { rawConfigToEntity, rawConfigUpdateToEntity, rawNativeContractToEntity, rawNetToEntity } from '../converters/config.converters'
+import { prismaClient } from '../lib/Setup'
+
 export const configRepository = {
-  findOne (query = {}, project = {}, collection) {
-    return collection.findOne(query, project)
-  },
-  find (query = {}, project = {}, collection, sort = {}, limit = 0, isArray = true) {
-    if (isArray) {
-      return collection
-        .find(query, project)
-        .sort(sort)
-        .limit(limit)
-        .toArray()
-    } else {
-      return collection
-        .find(query, project)
-        .sort(sort)
-        .limit(limit)
+  async findOne (query = {}, project = {}, collection) {
+    let existingConfig = await prismaClient.explorer_config.findFirst({
+      where: { id: query._id },
+      select: {
+        hash: true
+      }
+    })
+
+    if (!existingConfig) return null
+
+    if (query._id === '_explorerInitialConfiguration') {
+      existingConfig = await prismaClient.explorer_config.findFirst({
+        where: { id: query._id },
+        select: {
+          native_contract: {
+            select: {
+              name: true,
+              address: true
+            }
+          },
+          net: {
+            select: {
+              id: true,
+              name: true
+            }
+          }
+        }
+      })
+
+      existingConfig.net = existingConfig.net.pop()
+
+      console.log({ pepe: JSON.stringify(existingConfig, null, 4) })
+
+      const nativeContracts = {}
+
+      Object.values(existingConfig.native_contract).forEach(contract => {
+        nativeContracts[contract.name] = contract.address
+      })
+
+      console.log({ pepe: JSON.stringify(nativeContracts, null, 0) })
+
+      existingConfig.nativeContracts = nativeContracts
+      delete existingConfig.native_contract
+
+      console.log({ pepe2: JSON.stringify(existingConfig, null, 4) })
     }
+
+    return existingConfig
   },
-  countDocuments (query = {}, collection) {
-    return collection.countDocuments(query)
+  async insertOne (data, collection) {
+    const newConfig = rawConfigToEntity(data)
+
+    await prismaClient.explorer_config.upsert({
+      where: { id: newConfig.id },
+      update: newConfig,
+      create: newConfig
+    })
+
+    if (data._id === '_explorerInitialConfiguration') {
+      const newNet = rawNetToEntity(data.net)
+      newNet.configId = newConfig.id
+
+      await prismaClient.net.upsert({
+        where: { id: newNet.id },
+        update: newNet,
+        create: newNet
+      })
+
+      const nativeContracts = Object.entries(data.nativeContracts)
+
+      nativeContracts.forEach(async ([name, address]) => {
+        const newNativeContract = rawNativeContractToEntity({
+          name: name,
+          address: address
+        })
+
+        newNativeContract.configId = newConfig.id
+
+        await prismaClient.native_contract.upsert({
+          where: { address: newNativeContract.address },
+          update: newNativeContract,
+          create: newNativeContract
+        })
+      })
+    }
+
+    await collection.insertOne(data)
+
+    return newConfig
   },
-  aggregate (aggregate, collection) {
-    return collection.aggregate(aggregate).toArray()
-  },
-  insertOne (data, collection) {
-    return collection.insertOne(data)
-  },
-  updateOne (filter, update, options = {}, collection) {
-    return collection.updateOne(filter, update, options)
+  async updateOne (filter, newData, options = {}, collection) {
+    const newConfig = rawConfigUpdateToEntity(newData.$set)
+
+    await prismaClient.explorer_config.update({
+      where: { id: filter._id },
+      data: newConfig
+    })
+
+    await collection.updateOne(filter, newData, options)
+
+    return newConfig
   }
 }
