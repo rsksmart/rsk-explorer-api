baaba4e6001885e5ba58b685e950ee4b3c64e73e
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-03-10 13:23:51 -0300

    fix: added back find/findOne to stats and status repositories since its used by api

--- src/repositories/stats.repository.js
+++ src/repositories/stats.repository.js
@@ -2,6 +2,23 @@ import { rawBridgeToEntity, rawCirculatingToEntity, rawStatsToEntity } from '../
 import {prismaClient} from '../lib/Setup'
 
 export const statsRepository = {
+  findOne (query = {}, project = {}, collection) {
+    return collection.findOne(query, project)
+  },
+  find (query = {}, project = {}, collection, sort = {}, limit = 0, isArray = true) {
+    if (isArray) {
+      return collection
+        .find(query, project)
+        .sort(sort)
+        .limit(limit)
+        .toArray()
+    } else {
+      return collection
+        .find(query, project)
+        .sort(sort)
+        .limit(limit)
+    }
+  },
   async insertOne (data, collection) {
     const newStats = rawStatsToEntity(data)
 
--- src/repositories/status.repository.js
+++ src/repositories/status.repository.js
@@ -2,6 +2,23 @@ import { rawStatusToEntity } from '../converters/status.converters'
 import {prismaClient} from '../lib/Setup'
 
 export const statusRepository = {
+  findOne (query = {}, project = {}, collection) {
+    return collection.findOne(query, project)
+  },
+  find (query = {}, project = {}, collection, sort = {}, limit = 0, isArray = true) {
+    if (isArray) {
+      return collection
+        .find(query, project)
+        .sort(sort)
+        .limit(limit)
+        .toArray()
+    } else {
+      return collection
+        .find(query, project)
+        .sort(sort)
+        .limit(limit)
+    }
+  },
   async insertOne (data, collection) {
     await prismaClient.status.create({ data: rawStatusToEntity(data) })
     await collection.insertOne(data)
