8720f3e6eb03c6849b28d916dd5c1802c764cb2e
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-05-31 13:23:53 -0300

    fix: fixed blockchain metrics retrieval when bridge is not present (sync process)

--- src/api/lib/getCirculatingSupply.js
+++ src/api/lib/getCirculatingSupply.js
@@ -3,17 +3,25 @@ import { bigNumberDifference, applyDecimals } from '../../lib/utils'
 import { addressRepository } from '../../repositories/address.repository'
 
 export default async function (collection, { bridge }) {
-  try {
-    const result = await addressRepository.findOne({ address: bridge })
-    if (!result) throw new Error('Missing bridge account from db')
-    const bridgeBalance = applyDecimals(result.balance, 18).toString(10)
-    let circulatingSupply = bigNumberDifference(TOTAL_SUPPLY, bridgeBalance).toString(10)
+  const bridgeAddress = await addressRepository.findOne({ address: bridge })
+
+  if (!bridgeAddress) {
     return {
-      circulatingSupply,
+      circulatingSupply: '0',
       totalSupply: TOTAL_SUPPLY,
-      bridgeBalance
+      bridgeBalance: '0x0'
     }
-  } catch (err) {
-    return Promise.reject(err)
+  } else {
+    try {
+      const bridgeBalance = applyDecimals(bridgeAddress.balance, 18).toString(10)
+      let circulatingSupply = bigNumberDifference(TOTAL_SUPPLY, bridgeBalance).toString(10)
+      return {
+        circulatingSupply,
+        totalSupply: TOTAL_SUPPLY,
+        bridgeBalance
+      }
+    } catch (err) {
+      return Promise.reject(err)
+    }
   }
 }
