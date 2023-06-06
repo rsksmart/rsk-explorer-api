9af0bd348213b773cc45c9966400352f860e2c37
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-06-06 18:06:37 -0300

    feat: updated deleteBlocks.js tool, added getBlocks.js tool

--- src/tools/deleteBlocks.js
+++ src/tools/deleteBlocks.js
@@ -1,59 +1,30 @@
-import dataSource from '../lib/dataSource.js'
-import { deleteBlockDataFromDb, getBlockFromDb } from '../services/classes/Block'
-import { deleteBlockSummaryFromDb, getBlockSummariesByNumber } from '../services/classes/BlockSummary'
-import { BlocksBase } from '../lib/BlocksBase'
-import { log, orange, reset, error, ansiCode } from '@rsksmart/rsk-js-cli'
+import { dataSource } from '../lib/dataSource'
+import { blockRepository } from '../repositories/block.repository'
 
-dataSource({ skipCheck: true }).then(async ({ db }) => {
-  const options = new BlocksBase(db)
-  const { collections } = options
-  const p = path => path.split('/').pop()
-  const help = () => {
-    const myName = p(process.argv[1])
-    log.info(`Use: ${p(process.argv[0])} ${myName} [blockNumber] | [fromBlock-toBlock]`)
-    log.info(`e.g. ${orange} ${myName} 400`)
-    log.info(`e.g. ${orange} ${myName} 400-456`)
-    process.exit(0)
-  }
+async function main () {
+  const fromBlock = parseInt(process.argv[2])
+  const toBlock = parseInt(process.argv[3])
 
-  let fromTo = process.argv[2]
-  let deleteSummary = process.argv.find(x => x === '--deleteSummary')
-  if (!fromTo) help()
-  fromTo = fromTo.split('-')
-  let [f, t] = fromTo
+  if (isNaN(fromBlock)) throw new Error('fromBlock param must be a number')
+  if (isNaN(toBlock)) throw new Error('toBlock param must be a number')
+  if (toBlock - fromBlock < 0) throw new Error('First number must be lower than the second one')
 
-  if (!f) help()
-  if (!t) t = f
-  if (isNaN(f) || isNaN(t)) help()
-  if (f > t) help()
+  await dataSource()
 
-  try {
-    let Q = []
-    while (t >= f) {
-      let b = await getBlockFromDb(t, collections.Blocks)
-      let color = ansiCode(Number(t.toString().split('').pop()) + 30)
-      if (b) {
-        let { hash, number } = b
-        console.log(`${reset} ${color} ● ● ● Removing block  ${number} ${hash}`)
-        Q.push(deleteBlockDataFromDb(b.hash, number, collections))
-      }
-      if (deleteSummary) {
-        if (b) {
-          Q.push(deleteBlockSummaryFromDb(b.hash, options.collections))
-        } else {
-          console.log(`${reset} ${color} ● ● ● Removing ALL summaries for blockNumber: ${t}`)
-          let summaries = await getBlockSummariesByNumber(t, collections)
-          if (summaries.length) {
-            for (let summary of summaries) {
-              Q.push(deleteBlockSummaryFromDb(summary.hash, collections))
-            }
-          }
-        }
-      }
-      t--
+  console.log(`Deleting blocks from ${fromBlock} to ${toBlock} (${toBlock - fromBlock} blocks) `)
+
+  const query = {
+    number: {
+      gte: fromBlock,
+      lte: toBlock
     }
-    Promise.all(Q).then(() => process.exit())
-  } catch (err) {
-    error(err)
   }
-})
+
+  const deletedBlocksCount = await blockRepository.deleteMany(query)
+
+  console.log('Finished.')
+  console.log({ deletedBlocksCount })
+  process.exit(0)
+}
+
+main()
---
+++ src/tools/getBlocks.js
@@ -0,0 +1,39 @@
+import dataSource from '../lib/dataSource.js'
+import Block from '../services/classes/Block'
+import BlocksBase from '../lib/BlocksBase'
+import nod3 from '../lib/nod3Connect.js'
+
+async function main () {
+  if (isNaN(process.argv[2])) throw new Error('fromBlock param is missing.')
+  const latestBlock = await nod3.eth.getBlock('latest')
+  const fromBlock = parseInt(process.argv[2])
+  const toBlock = process.argv[3] ? parseInt(process.argv[3]) : latestBlock.number
+
+  let blockToSave = fromBlock
+  try {
+    console.log(`Getting blocks from ${fromBlock} to ${toBlock} (${toBlock - fromBlock} blocks)`)
+
+    const { db, initConfig } = await dataSource()
+
+    while (blockToSave <= toBlock) {
+      let timestamp = Date.now()
+
+      // insert block
+      const block = new Block(blockToSave, new BlocksBase(db, { initConfig }))
+      await block.fetch()
+      await block.save()
+      timestamp = Date.now() - timestamp
+
+      console.log(`Block ${blockToSave} saved! (${timestamp} ms)`)
+      blockToSave++
+    }
+  } catch (error) {
+    console.log(`Error saving block ${blockToSave}.`)
+    console.log(error)
+  }
+
+  console.log('Finished.')
+  process.exit(0)
+}
+
+main()
