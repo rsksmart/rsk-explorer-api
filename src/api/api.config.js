653cf4f1a65c574ab18722476ec7df0eaa24a925
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-06-30 14:23:11 -0300

    feat: now api logs are stored in logs/api (inside project)

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -1,18 +1,11 @@
-import config from '../lib/config'
-
-const cwd = `${__dirname}`
 const name = 'explorer-api'
-const script = 'index.js'
-const { log } = config
 
-const conf = { script, name, cwd }
-
-if (log && log.dir) {
-  let { dir } = log
-  conf.error_file = `${dir}/${name}-error.log`
-  conf.out_file = `${dir}/${name}-out.log`
+const conf = {
+  name,
+  script: 'index.js',
+  cwd: __dirname,
+  error_file: `../../logs/api/${name}-error.log`,
+  out_file: `../../logs/api/${name}-out.log`
 }
 
 export const apps = [conf]
-
-console.log(apps)
