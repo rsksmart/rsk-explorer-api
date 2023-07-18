32c05ade0882f1b62caaa4526b13becd3f6827cd
Dario <dario.gomez@iovlabs.org>
2023-07-18 16:49:49 -0300

    fix: added default log object in param

--- src/lib/dataSource.js
+++ src/lib/dataSource.js
@@ -1,6 +1,6 @@
 
 import Setup from './Setup'
-export const dataSource = async ({ log, skipCheck } = {}) => {
+export const dataSource = async ({ log = console, skipCheck } = {}) => {
   try {
     const setup = await Setup({ log })
     return setup.start(skipCheck)
