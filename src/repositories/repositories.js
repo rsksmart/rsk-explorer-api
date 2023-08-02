f1297089c10a929470a486b5be8b367d61868ac3
Dario <dario.gomez@iovlabs.org>
2023-08-02 17:38:23 -0300

    fix: refactor how centralized repositories object imports repositories

--- src/repositories/repositories.js
+++ src/repositories/repositories.js
@@ -1,10 +1,27 @@
-import fs from 'fs'
+import { addressRepository } from './address.repository'
+import { balancesRepository } from './balances.repository'
+import { blockRepository } from './block.repository'
+import { blockTraceRepository } from './blockTrace.repository'
+import { eventRepository } from './event.repository'
+import { internalTxRepository } from './internalTx.repository'
+import { summaryRepository } from './summary.repository'
+import { statsRepository } from './stats.repository'
+import { statusRepository } from './status.repository'
+import { tokenRepository } from './token.repository'
+import { txRepository } from './tx.repository'
+import { txPoolRepository } from './txPool.repository'
 
-export const REPOSITORIES = fs.readdirSync(__dirname).reduce((repos, repo) => {
-  const [name] = repo.split('.')
-  const [requiredRepo] = Object.values(require(`${__dirname}/${repo}`))
-
-  repos[name[0].toUpperCase() + name.slice(1)] = requiredRepo
-
-  return repos
-}, {})
+export const REPOSITORIES = {
+  Address: addressRepository,
+  Balances: balancesRepository,
+  Block: blockRepository,
+  BlockTrace: blockTraceRepository,
+  Event: eventRepository,
+  InternalTx: internalTxRepository,
+  Stats: statsRepository,
+  Status: statusRepository,
+  Summary: summaryRepository,
+  Token: tokenRepository,
+  Tx: txRepository,
+  TxPool: txPoolRepository
+}
