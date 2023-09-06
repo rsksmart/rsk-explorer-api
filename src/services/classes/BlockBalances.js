acc0c9b5590a3ac9a991770d96fc3a7f57307010
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-09-06 19:53:55 -0300

    wip: store latest addresses balances in new table

--- src/services/classes/BlockBalances.js
+++ src/services/classes/BlockBalances.js
@@ -1,5 +1,7 @@
 export async function fetchAddressesBalancesFromNode (addresses, block, nod3) {
   const balances = []
+  const latestBalances = []
+  const parseBalance = (balance) => (parseInt(balance)) ? balance : 0
   if (addresses && addresses.length && block && nod3) {
     const { hash: blockHash, number: blockNumber, timestamp } = block
 
@@ -7,14 +9,21 @@ export async function fetchAddressesBalancesFromNode (addresses, block, nod3) {
       const balance = await nod3.eth.getBalance(address, blockNumber)
       balances.push({
         address,
-        balance: (parseInt(balance)) ? balance : 0,
+        balance: parseBalance(balance),
         blockHash,
         blockNumber,
         timestamp,
         _created: Date.now()
       })
+
+      const latestBalance = await nod3.eth.getBalance(address, 'latest')
+      latestBalances.push({
+        address,
+        blockNumber,
+        balance: parseBalance(latestBalance)
+      })
     }
   }
 
-  return balances
+  return { balances, latestBalances }
 }
