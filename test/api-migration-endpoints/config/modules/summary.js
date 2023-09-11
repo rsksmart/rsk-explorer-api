61d6d8461d31d728eed7dd387c84f183fd13f4b5
Dario <dario.gomez@iovlabs.org>
2023-09-10 21:28:02 -0300

    fix: fix summary fixtures

--- test/api-migration-endpoints/config/modules/summary.js
+++ test/api-migration-endpoints/config/modules/summary.js
@@ -6,9 +6,9 @@ const endpoints = {
 const fixtures = {
   testnet: {
     blockHashesForGetSummaryEndpoint: [
-      '0xafe0a6a3710d9c80d2873f3bfe8be06dee85ac467baf5a9fcc8954dccb39029d',
-      '0x2459cec68e7e0e2dd84fc1bb8f0b01abb3316eabe74f07657f5fd7b366b153a3',
-      '0x532252e56396effb714e20c4ab55106eb06021aad131e118083522f87e673122'
+      '0x3018a56eb41bd1e0aa0b037cec82abebd36d8373629d31253f45a04b2695ffc5',
+      '0xb33e25877e1f8bf7b427905b2e829b5be5946fdeaafc2ea580b488ccf059e6a4',
+      '0x80c5ec2d8b86b0da7760a8564a9a6c76b97002989b33bcc428aa46b3abd151a0'
     ]
   },
   mainnet: {}
