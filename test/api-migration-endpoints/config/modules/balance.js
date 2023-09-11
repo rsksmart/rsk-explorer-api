d8e1cf471defe77c591776acaf251cfc43dfc894
Dario <dario.gomez@iovlabs.org>
2023-09-10 21:28:38 -0300

    fix: fix balance fixtures

--- test/api-migration-endpoints/config/modules/balance.js
+++ test/api-migration-endpoints/config/modules/balance.js
@@ -7,9 +7,9 @@ const endpoints = {
 const fixtures = {
   testnet: {
     blockNumbersAndAddressesForGetBalanceEndpoint: [
-      { address: '0x6ef810908a2a75577926133799d19b7d37bedd81', block: 4100019 },
-      { address: '0x479a664a4bf82dfd4cd04a9c6a4dc41fc5666a28', block: 4100020 },
-      { address: '0xa94e301a7aa62ce1b37a2aa2370f1f4746a22316', block: 4100020 }
+      { address: '0xb774aa2876145b2f6f3de27e5e6ac970aa12d771', block: 4092555 },
+      { address: '0x0000000000000000000000000000000001000008', block: 10506 },
+      { address: '0xf83f3647aa097fb8bdd74206e498b9c0c67f72ab', block: 900001 }
     ],
     addressesForGetBalancesEndpoint: [
       '0x6ef810908a2a75577926133799d19b7d37bedd81',
