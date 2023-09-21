038ea06107dcbb314621464f5fbf01b90e85d01c
Dario <dario.gomez@iovlabs.org>
2023-09-21 17:24:18 -0300

    fix: add missing find method in verificationResults repository

--- src/repositories/verificationResults.repository.js
+++ src/repositories/verificationResults.repository.js
@@ -2,6 +2,11 @@ import { verificationResultsEntityToRaw, rawVerificationResultsToEntity } from '
 
 export function getVerificationResultsRepository (prismaClient) {
   return {
+    async find (query) {
+      const verificationResults = await prismaClient.verification_result.findMany({ where: query })
+
+      return verificationResults.map(verificationResultsEntityToRaw)
+    },
     async findOne (query) {
       const verificationResult = await prismaClient.verification_result.findFirst({ where: query })
 
