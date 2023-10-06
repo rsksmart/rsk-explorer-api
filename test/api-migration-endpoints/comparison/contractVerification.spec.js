210bd1e081439e7f009e6f51030a1765cb835c1e
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-10-06 13:44:48 -0300

    fix: update contractVerification tests

--- test/api-migration-endpoints/comparison/contractVerification.spec.js
+++ test/api-migration-endpoints/comparison/contractVerification.spec.js
@@ -7,25 +7,39 @@ const { network } = config
 
 const {
   getVerifiedContracts,
-  // verify, TODO
+  // verify,
   getSolcVersions,
   getEvmVersions,
-  getVerificationResult,
+  // getVerificationResult,
   isVerified
 } = endpoints
 
 const {
-  idsForGetVerificationResultEndpoint,
-  addressesForGetIsVerifiedEndpoint
+  addressesForIsVerifiedEndpoint
 } = fixtures[network]
+
+const keysToSkipForContractVerification = {
+  data: ['_id', ['request', '_id'], 'timestamp']
+}
 
 describe('ContractVerification module', () => {
   describe('GET getVerifiedContracts endpoint', () => {
     const endpoint = getVerifiedContracts()
     it(sameDataMsg(endpoint), async () => {
-      await compareDataFromBothEnvs({ endpoint })
+      await compareDataFromBothEnvs({
+        endpoint,
+        keysToSkip: keysToSkipForContractVerification
+      })
     })
   })
+
+  // TODO: Check verify endpoint behaviour, then perform this test
+  // describe('GET verify endpoint', () => {
+  //   const endpoint = verify()
+  //   it(sameDataMsg(endpoint), async () => {
+  //     await compareDataFromBothEnvs({ endpoint })
+  //   })
+  // })
 
   describe('GET getSolcVersions endpoint', () => {
     const endpoint = getSolcVersions()
@@ -41,20 +55,25 @@ describe('ContractVerification module', () => {
     })
   })
 
-  describe('GET getVerificationResult endpoint', () => {
-    for (const id of idsForGetVerificationResultEndpoint) {
-      const endpoint = getVerificationResult({ id })
-      it(sameDataMsg(endpoint), async () => {
-        await compareDataFromBothEnvs({ endpoint })
-      })
-    }
-  })
+  // TODO: Check how to perform this test
+  // Requests are not unique, also ids for the same address will differ (diff timestamp)
+  // describe('GET getVerificationResult endpoint', () => {
+  //   for (const id of idsForGetVerificationResultEndpoint) {
+  //     const endpoint = getVerificationResult({ id })
+  //     it(sameDataMsg(endpoint), async () => {
+  //       await compareDataFromBothEnvs({ endpoint })
+  //     })
+  //   }
+  // })
 
-  describe('GET getVerification endpoint', () => {
-    for (const address of addressesForGetIsVerifiedEndpoint) {
+  describe('GET isVerified endpoint', () => {
+    for (const address of addressesForIsVerifiedEndpoint) {
       const endpoint = isVerified({ address })
       it(sameDataMsg(endpoint), async () => {
-        await compareDataFromBothEnvs({ endpoint })
+        await compareDataFromBothEnvs({
+          endpoint,
+          keysToSkip: keysToSkipForContractVerification
+        })
       })
     }
   })
--- test/api-migration-endpoints/config/modules/contractVerification.js
+++ test/api-migration-endpoints/config/modules/contractVerification.js
@@ -1,22 +1,20 @@
 const endpoints = {
   getVerifiedContracts: () => '/api?module=contractVerifier&action=getVerifiedContracts',
-  verify: () => `/api?module=contractVerifier&action=verify`, // TODO: do something
+  verify: () => `/api?module=contractVerifier&action=verify`,
   getSolcVersions: () => '/api?module=contractVerifier&action=getSolcVersions',
   getEvmVersions: () => '/api?module=contractVerifier&action=getEvmVersions',
   getVerificationResult: ({ id }) => `/api?module=contractVerifier&action=getVerificationResult&id=${id}`,
-  isVerified: ({ address, fields }) => {
-    return `/api?module=contractVerifier&action=isVerified&address=${address}${fields ? `&fields=${fields}` : ''}`
-  }
+  isVerified: ({ address, fields }) => `/api?module=contractVerifier&action=isVerified&address=${address}${fields ? `&${fields}` : ''}`
 }
 
 const fixtures = {
-  // the db still needs to be populated with contractVerification data
   testnet: {
-    idsForGetVerificationResultEndpoint: [
-
-    ],
-    addressesForGetIsVerifiedEndpoint: [
-
+    addressesForIsVerifiedEndpoint: [
+      // Requisites:
+      // Store blocks 4314259, 4314394
+      // Verify contracts
+      '0xc8069f0ecaad27b408bc59632644eba6aa249dc6',
+      '0xfc0be0c9e865424edebdfc88bfe1b788e5f755ec'
     ]
   },
   mainnet: {}
