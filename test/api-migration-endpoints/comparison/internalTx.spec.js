5c9c87772c9ea4400175632dbb180409c29108b2
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-10-06 17:26:22 -0300

    fix: fix internalTx tests nesting

--- test/api-migration-endpoints/comparison/internalTx.spec.js
+++ test/api-migration-endpoints/comparison/internalTx.spec.js
@@ -36,63 +36,63 @@ describe('InternalTx module', () => {
         })
       })
     }
+  })
 
-    describe('GET getInternalTransactions endpoint', () => {
-      const endpoint = getInternalTransactions()
+  describe('GET getInternalTransactions endpoint', () => {
+    const endpoint = getInternalTransactions()
+    it(sameDataMsg(endpoint), async () => {
+      await compareDataFromBothEnvs({
+        endpoint: endpoint + '&limit=200',
+        keysToSkip: keysToSkipForInternalTx
+      })
+    })
+  })
+
+  describe('GET getInternalTransactionsByAddress endpoint', () => {
+    for (const address of addressesForGetInternalTransactionsByAddressEndpoint) {
+      const endpoint = getInternalTransactionsByAddress({ address })
       it(sameDataMsg(endpoint), async () => {
         await compareDataFromBothEnvs({
-          endpoint: endpoint + '&limit=200',
+          endpoint,
           keysToSkip: keysToSkipForInternalTx
         })
       })
-    })
+    }
+  })
 
-    describe('GET getInternalTransactionsByAddress endpoint', () => {
-      for (const address of addressesForGetInternalTransactionsByAddressEndpoint) {
-        const endpoint = getInternalTransactionsByAddress({ address })
-        it(sameDataMsg(endpoint), async () => {
-          await compareDataFromBothEnvs({
-            endpoint,
-            keysToSkip: keysToSkipForInternalTx
-          })
+  describe('GET getInternalTransactionsByBlock endpoint', () => {
+    for (const hash of blockHashesForGetInternalTransactionsByBlockEndpoint) {
+      const endpoint = getInternalTransactionsByBlock({ hashOrNumber: hash })
+      it(sameDataMsg(endpoint), async () => {
+        await compareDataFromBothEnvs({
+          endpoint,
+          keysToSkip: keysToSkipForInternalTx
         })
-      }
-    })
+      })
+    }
 
-    describe('GET getInternalTransactionsByBlock endpoint', () => {
-      for (const hash of blockHashesForGetInternalTransactionsByBlockEndpoint) {
-        const endpoint = getInternalTransactionsByBlock({ hashOrNumber: hash })
-        it(sameDataMsg(endpoint), async () => {
-          await compareDataFromBothEnvs({
-            endpoint,
-            keysToSkip: keysToSkipForInternalTx
-          })
+    const availableParams = ['hashOrNumber', 'number']
+    for (const number of blockNumbersForGetInternalTransactionsByBlockEndpoint) {
+      const endpoint = getInternalTransactionsByBlock({ [availableParams[Math.floor(Math.random() * 2)]]: number })
+      it(sameDataMsg(endpoint), async () => {
+        await compareDataFromBothEnvs({
+          endpoint,
+          keysToSkip: keysToSkipForInternalTx
         })
-      }
+      })
+    }
+  })
 
-      const availableParams = ['hashOrNumber', 'number']
-      for (const number of blockNumbersForGetInternalTransactionsByBlockEndpoint) {
-        const endpoint = getInternalTransactionsByBlock({ [availableParams[Math.floor(Math.random() * 2)]]: number })
-        it(sameDataMsg(endpoint), async () => {
-          await compareDataFromBothEnvs({
-            endpoint,
-            keysToSkip: keysToSkipForInternalTx
-          })
+  describe('GET getInternalTransactionsByTxHash endpoint', () => {
+    const availableParams = ['transactionHash', 'hash']
+    for (const hash of txHashesForGetInternalTransactionsByTxHashEndpoint) {
+      const endpoint = getInternalTransactionsByTxHash({ [availableParams[Math.floor(Math.random() * 2)]]: hash })
+      it(sameDataMsg(endpoint), async () => {
+        await compareDataFromBothEnvs({
+          endpoint,
+          keysToSkip: keysToSkipForInternalTx
         })
-      }
-    })
-
-    describe('GET getInternalTransactionsByTxHash endpoint', () => {
-      const availableParams = ['transactionHash', 'hash']
-      for (const hash of txHashesForGetInternalTransactionsByTxHashEndpoint) {
-        const endpoint = getInternalTransactionsByTxHash({ [availableParams[Math.floor(Math.random() * 2)]]: hash })
-        it(sameDataMsg(endpoint), async () => {
-          await compareDataFromBothEnvs({
-            endpoint,
-            keysToSkip: keysToSkipForInternalTx
-          })
-        })
-      }
-    })
+      })
+    }
   })
 })
