16b993d3b7d630f8c1d65e5f7553a545f125e7e5
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2023-10-22 03:36:16 -0300

    fix: remove unnecessary setup instance

--- src/services/userEvents/userEventsService.js
+++ src/services/userEvents/userEventsService.js
@@ -5,54 +5,51 @@ import { serialize } from '../../lib/utils'
 import AddressModule from './AddressModule' */
 import ContractVerifierModule from './ContractVerifierModule'
 import { errors } from '../../lib/types'
-import { Setup } from '../../lib/Setup'
 
 const log = Logger('[user-events-service]', config.blocks.log)
-Setup({ log })
-  .start({ skipCheck: true })
-  .then(() => {
-    const verifierConfig = config.api.contractVerifier
-    // const initConfig = JSON.parse(process.argv[2])
-    // const cache = new RequestCache()
-    // TODO, conditional creation
-    const verifierModule = ContractVerifierModule(verifierConfig, { log })
-    // const addressModule = AddressModule({ db, collections, initConfig, log })
 
-    process.on('message', async msg => {
-      const sendMessage = (msg) => process.send(serialize(msg))
+const verifierConfig = config.api.contractVerifier
+// const initConfig = JSON.parse(process.argv[2])
+// const cache = new RequestCache()
+// TODO, conditional creation
+const verifierModule = ContractVerifierModule(verifierConfig, { log })
+// const addressModule = AddressModule({ db, collections, initConfig, log })
 
-      try {
-        // let { action, params, block, module } = msg
-        let { action, module } = msg
-        if (module && action) {
-          switch (module) {
-            // Address module
-            /*          case 'Address':
-                        if (action === 'updateAddress') {
-                          if (!block) return
-                          msg = await addressModule.updateAddress({ cache, msg }, params)
-                          sendMessage(msg)
-                        }
-                        break */
-            // Contract Verifier module
-            case 'ContractVerification':
-              const method = verifierModule[action]
-              if (!method) throw new Error(`Unknown action ${action}`)
-              try {
-                msg = await method(msg)
-                sendMessage(msg)
-              } catch (err) {
-                log.debug(err)
-                msg.error = errors.TEMPORARILY_UNAVAILABLE
-                sendMessage(msg)
-                throw err
-              }
-              break
+process.on('message', async msg => {
+  const sendMessage = (msg) => process.send(serialize(msg))
+
+  try {
+    // let { action, params, block, module } = msg
+    let { action, module } = msg
+    if (module && action) {
+      switch (module) {
+        // Address module
+        /*          case 'Address':
+                    if (action === 'updateAddress') {
+                      if (!block) return
+                      msg = await addressModule.updateAddress({ cache, msg }, params)
+                      sendMessage(msg)
+                    }
+                    break */
+        // Contract Verifier module
+        case 'ContractVerification':
+          const method = verifierModule[action]
+          if (!method) throw new Error(`Unknown action ${action}`)
+          try {
+            msg = await method(msg)
+            sendMessage(msg)
+          } catch (err) {
+            log.debug(err)
+            msg.error = errors.TEMPORARILY_UNAVAILABLE
+            sendMessage(msg)
+            throw err
           }
-        }
-      } catch (err) {
-        log.error(err)
+          break
       }
-    })
-    log.info('Online')
-  })
+    }
+  } catch (err) {
+    log.error(err)
+  }
+})
+
+log.info('Online')
