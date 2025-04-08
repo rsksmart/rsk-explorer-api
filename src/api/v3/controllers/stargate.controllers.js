6e5db6c18faf660f5bebc22daa3756f881b0a2e8
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2025-04-08 16:26:11 -0300

    refactor: normalize controller errors

--- src/api/v3/controllers/stargate.controllers.js
+++ src/api/v3/controllers/stargate.controllers.js
@@ -5,15 +5,16 @@ import { isAddress } from '@rsksmart/rsk-utils/dist/addresses'
 
 const log = Logger('[v3.stargate.controllers]')
 
+const ERRORS = {
+  INVALID_ADDRESS: 'A valid address is required',
+  INTERNAL_SERVER_ERROR: 'Internal server error'
+}
+
 export const validateStargateAddress = async (req, res) => {
   try {
     const { address } = req.params
 
-    if (!address || !isAddress(address)) {
-      return res.status(400).send({
-        error: 'A valid address is required'
-      })
-    }
+    if (!address || !isAddress(address)) throw new Error(ERRORS.INVALID_ADDRESS)
 
     const balance = await getAddressBalance(address)
     const rbtcPrice = await getRBTCPrice()
@@ -35,14 +36,24 @@ export const validateStargateAddress = async (req, res) => {
         assetsValueInUSDT,
         isEligible: assetsValueInUSDT > config.api.stargate.minAssetsValueThresholdInUSDT,
         minAssetsValueThresholdInUSDT: config.api.stargate.minAssetsValueThresholdInUSDT
-      }
+      },
+      error: false,
+      errorMessage: null
     })
   } catch (error) {
     log.error('Error validating stargate address')
     log.error(error)
 
+    if (error.message === ERRORS.INVALID_ADDRESS) {
+      return res.status(400).send({
+        error: true,
+        errorMessage: ERRORS.INVALID_ADDRESS
+      })
+    }
+
     return res.status(500).send({
-      error: 'Internal server error'
+      error: true,
+      errorMessage: ERRORS.INTERNAL_SERVER_ERROR
     })
   }
 }
